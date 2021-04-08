d8516bedc9874987f9dde713da046e8c2ff7d03e
tina190666 <51132204+tina190666@users.noreply.github.com>
2021-04-08 15:09:19 -0700

    Update Function.ino
    Added LCD, Organized time check and updated time period to 10min, removed vacc_Log

--- Function/Function.ino
+++ Function/Function.ino
@@ -4,67 +4,43 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // you can also call it with a different address you want
 //Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 
-//declare the 7 servos (1 per conveyor belt)
-// Servo servo1;
-// Servo servo2;
-// Servo servo3;
-// Servo servo4;
-// Servo servo5;
-// Servo servo6;
-// Servo servo7;
 
-File vacc_Log;
 File temp_Log;
 
-int vacc_in = 0;
-int vacc_out = 0;
 int col = 11;
 int total = 1001;
 int layer = 1;
 int row = 13;
 int count_col = 0;
 long prevMilli = 0;
-long period = 10800000; //3 hours: 3*60*60*1000
+long period = 600000; //10 minutes: 1000*60*10 = 600000
 
 void setup() {
   Serial.begin(9600);  //Initialize serial monitor for user input and instructional output.
 
-  pinMode(10, INPUT);     //Setting pin 10 as the input pin for the sliding switch controlling the base of the catch compartment.
-
-  pinMode(11, INPUT_PULLUP);  //Setting pin 11 as the input pin for the pushbutton that represents the state of the retrieval compartment (opened / closed)
-
-  pinMode(12, OUTPUT);      //Setting pin 12 as the output pin for the red LED that indicates the thermal state of the vaccines.
-
-  pinMode(13, OUTPUT);     //Setting pin 23 as the output pin for the blue LED that indicates the access state of the retrieval compartment.
-
+  pinMode(6, INPUT);     //Setting pin 10 as the input pin for the sliding switch controlling the base of the catch compartment.
+  
+  pinMode(7, INPUT_PULLUP);  //Setting pin 11 as the input pin for the pushbutton that represents the state of the retrieval compartment (opened / closed)
+       
+  pinMode(8, OUTPUT);      //Setting pin 12 as the output pin for the red LED that indicates the thermal state of the vaccines.
+                        
+  pinMode(9, OUTPUT);     //Setting pin 23 as the output pin for the blue LED that indicates the access state of the retrieval compartment.
+  
   pinMode(A0, INPUT);   //Setting Analog 0 pin as the input pin where the sensor opamp of the sensor will be connected to.
 
- //Servos
- /*
-  * for each servo:
-  *   attach servo to a pin on Arduino
-  *   setting the initial position of the servo to 0 degrees
-  */
-  // servo1.attach(3, 500, 2500);
-  // servo1.write(0);
-  // servo2.attach(4, 500, 2500);
-  // servo2.write(0);
-  // servo3.attach(5, 500, 2500);
-  // servo3.write(0);
-  // servo4.attach(6, 500, 2500);
-  // servo4.write(0);
-  // servo5.attach(7, 500, 2500);
-  // servo5.write(0);
-  // servo6.attach(8, 500, 2500);
-  // servo6.write(0);
-  // servo7.attach(9, 500, 2500);
-  // servo7.write(0);
+  lcd.begin(16, 2); //intialize LCD
+  lcd.print("Vaccine Count: "); //print Vaccine Count: on the first row of the LCD
 
   // if you want to really speed stuff up, you can go into 'fast 400khz I2C' mode
 // some i2c devices dont like this so much so if you're sharing the bus, watch
@@ -88,7 +64,6 @@ void loop(){
     while(1);
   }
   Serial.println("Card Initialized");     //message printed when SD card is successfully initialized
-  vacc_Log = SD.open("Vaccine_Count.txt", FILE_WRITE);   //open vacc_Log file to store the number of doses available in the shipper.
   temp_Log = SD.open("Temperature.txt", FILE_WRITE);    //open temp_log file to store the temperature of vaccines every 3 hours.
 
   if(vacc_Log && temp_Log){          //when both files are successfully opened
@@ -96,16 +71,17 @@ void loop(){
     float temperature = convert(volt_analog);  //converts analogue input to the temperature of the vaccines
     bool tempThresh =  ThresholdTemp(temperature);   //checks if the temperature is above -60 C
     long milli = millis();                         //initiates time variable
-    if(milli - prevMilli >= period){              //if the current time is more than or equal to 3 hours away from the last time temperature data was logged
-      tempLog(milli, temperature, temp_Log);      //log the temperature of the vaccines to temp_Log file and set current time to prevMilli
-      prevMilli = milli;
+    bool logTime = LogTime(milli, prevMilli, period); //checks if the current time is more than or equal to 10 minutes away from the last time temperature data was logged
+    if(logTime){                              //if the current time is more than or equal to 10 minutes away from the last time temperature data was logged
+      tempLog(milli, temperature, temp_Log);    //log the temperature of the vaccines to temp_Log file and set current time to prevMilli
+       prevMilli = milli;
     }
     redLED(tempThresh, 1);                        //turn on red led to alert user of the vaccine's thermal stability being at risk if temperature of the vaccines is greater than -60C
-    if(digitalRead(1) == LOW){                    //if vaccines are thermally stable
+    if(!tempThresh){                    //if vaccines are thermally stable
       while(Serial.available()){                  //when the user inputs the desired number of doses to be withdrawn
-        vacc_in = Serial.parseInt();              //parse the input from String to int
+        int vacc_in = Serial.parseInt();              //parse the input from String to int  
         int vacc_out = vaccine_out(vacc_in, total, col);  //calculate the closest number of vaccines to the input that can be output from the shipper.
-        vaccineLog(vacc_out, total, millis(), vacc_Log);  //log the number of doses left in the shipper after the determined amount is withdrawn along with the current time
+        vaccineDisplay(vacc_out, total, lcd);  //display the number of doses left in the shipper after the determined amount is withdrawn on the LCD
         int rotation = (vacc_out, col);          //calculate the number of rotations of the servo required to dispense vaccines
 
         //Initialize variables to keep track of number of rotations and the number of rows dispensed since the first withdrawal.
@@ -123,53 +99,50 @@ void loop(){
             layer++;                              //if the layer is empty, increment layer by 1 (for next round of rotation or withdrawal)
           }
 
-          bool catch_full = catchFull(count_catch); //check if the catch compartment is full (11 * 13 = 143 doses)
-
-          if(catch_full || count_rot == rotation){   //if the catch compartment is full or if all required doses is in the catch compartment
-            boolean state = true;
-            blueLED(state, 13);                   //turn on the blue LED to indicate catch compartment is ready to drop vaccines into the retrieval compartment
-            catchSlide(10);                       //slide the base of the catch compartment, allowing the vaccines to drop into the retrieval compartment
+        if(catch_full || count_rot == rotation){   //if the catch compartment is full or if all required doses is in the catch compartment
+            boolean state = true;          
+            blueLED(state, 9);                   //turn on the blue LED to indicate catch compartment is ready to drop vaccines into the retrieval compartment
+            catchSlide(6);                       //slide the base of the catch compartment, allowing the vaccines to drop into the retrieval compartment
             while(!accessState()){      //when the retrieval compartment is not opened by the user
               blinkBlue(11);            //blink the blue LED to alert the user to pick up vaccines from the retreival compartment
             }
-
-            blueLED(accessState(), 13); //blueLED stays on when retrieval compartment is opened.
-
+            
+            blueLED(accessState(), 9); //blueLED stays on when retrieval compartment is opened.
+            
             while(accessState()){     //while the retreival compartment is opened:
               //measure data from temperature sensor and convert to the temperature of the vaccines
-              volt_analog = measure();
+              volt_analog = measure();                    
               temperature = convert(volt_analog);
               tempThresh = ThresholdTemp(temperature);
               milli = millis();
-              //log temperature of the vaccines if the current time is 3 hours or longer away from the last time the data was logged
-              if(milli - prevMilli >= period){
-               tempLog(milli, temperature, temp_Log);
-               prevMilli = milli;
+              bool logTime = LogTime(milli, prevMilli, period); //checks if the current time is more than or equal to 10 minutes away from the last time temperature data was logged
+              if(logTime){                              //if the current time is more than or equal to 10 minutes away from the last time temperature data was logged
+                tempLog(milli, temperature, temp_Log);    //log the temperature of the vaccines to temp_Log file and set current time to prevMilli
+                prevMilli = milli;
               }
             }
 
-
-            while(digitalRead(1) == HIGH){   //after the retrieval compartment is closed by the user and if the temperature of vaccines is above -60C; looped until the vaccine temperature is within the optimal range.
+            
+            while(tempThresh){   //after the retrieval compartment is closed by the user and if the temperature of vaccines is above -60C; looped until the vaccine temperature is within the optimal range.
               //measure data from temperature sensor and convert to the temperature of the vaccines
               volt_analog = measure();
               temperature = convert(volt_analog);
               tempThresh = ThresholdTemp(temperature);
               milli = millis();
-              //log temperature of the vaccines if the current time is 3 hours or longer away from the last time the data was logged
-              if(milli - prevMilli >= period){
-               tempLog(milli, temperature, temp_Log);
-               prevMilli = milli;
+              bool logTime = LogTime(milli, prevMilli, period); //checks if the current time is more than or equal to 10 minutes away from the last time temperature data was logged
+              if(logTime){                              //if the current time is more than or equal to 10 minutes away from the last time temperature data was logged
+                tempLog(milli, temperature, temp_Log);    //log the temperature of the vaccines to temp_Log file and set current time to prevMilli
+                prevMilli = milli;
               }
             }
             count_catch = 0;
           }
 
-        }
+         }
       }
     }
 
-    //close vacc_Log and temp_Log files.
-    vacc_Log.close();
+    //close temp_Log file.
     temp_Log.close();
   }
 
