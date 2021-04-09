13f8d9160b00c2cf5d5cb127c526f3d45ee9bd82
tina190666 <51132204+tina190666@users.noreply.github.com>
2021-04-09 14:52:23 -0700

    Update Function.h
    Updated Comments

--- Function/Function.h
+++ Function/Function.h
@@ -5,18 +5,21 @@
  *      Author: tina
  */
 #include <Servo.h>
+#include "Function.h"
 #include <SD.h>
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 #include <LiquidCrystal.h>
 
 #ifndef FUNCTION_H
 extern "C" {
   #define FUNCTION_H_
 
-  //measure function that performs the analogRead and returns the value.
+  //measure function that performs the analogRead from temperature sensor and returns the value.
   int measure();
 
-  //convert function that takes the analogRead and converts it to temperature.
+  //convert function that takes the analogRead value from the sensor and converts it to temperature.
   float convert(int volt_analog);
 
   //ThresholdTemp function that checks if the temperature of the vaccines is greater than its optimal temperature range.
@@ -34,13 +37,13 @@ extern "C" {
   //blinkBlue function that blinks the blue LED when retrieval compartment is not opened but ready to to be opened.
   void blinkBlue(int pinNumber);
 
-  //pos function that rotates the conveyor belt of the lowest non-empty layer in the shipper by 13.85 degrees (dispenses one row)
+  //pos function that rotates the conveyor belt of the lowest non-empty layer in the shipper by 13.85 degrees (dispenses one column)
   void pos(int layer, Adafruit_PWMServoDriver pwm, int col_count);
 
   //tempLog function that logs the temperature (celcius) and time (hours) into file in SD card.
   void tempLog(long milli, float temperature, File tempFile);
 
-  //vaccineLog function that diplays the number of vaccines remaining in the shipper to LCD.
+  //vaccineDisplay function that diplays the number of vaccines remaining in the shipper to LCD.
   void vaccineDisplay(int vacc_out, int total, LiquidCrystal lcd);
 
   //vaccine_out function that calcualtes and returns the closest number of vaccines withdrawable by the shipper (units of 11 doses) to the request of the user.
